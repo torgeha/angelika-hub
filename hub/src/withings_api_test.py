ea6d63a8414158e8ba7353a1bde037747e4a469f
Daveiac <daveiac@hotmail.com>
2014-10-09 13:02:10 +0200

    removed sensitive data from withings_api_test.py

--- hub/src/withings_api_test.py
+++ hub/src/withings_api_test.py
@@ -1,25 +1,28 @@
 from withings import *
+import ConfigParser
+import os
+os.chdir('../res')  # This is to access the res directory in hub
 
-access_token = "62c93288933174c5e60c24d3d71bc53d4c51939296cb1f760e036f6355370c"
-access_token_secret = "d30ccf3bb4c198deb26762f1d8d2c1d47e5c76d073e4525de2857f751f4266"
-consumer_key = "62f96b70bb38711bc7753f94ef78e991e8df4304bd114220ed83b260e43c423"
-consumer_secret = "f5c8d22e495d0d24f0d07952ecb5489e711f5dfd315d6fb7d67598532326"
-user_id = "4690424"
+config = ConfigParser.RawConfigParser()
+config.read('oauth_pulse02.txt')
+
+access_token = config.get('keys', 'oauth_token')
+access_token_secret = config.get('keys', 'oauth_token_secret')
+consumer_key = config.get('keys', 'consumer_key')
+consumer_secret = config.get('keys', 'consumer_secret')
+user_id = config.get('keys', 'userid')
 
 creds = WithingsCredentials(access_token, access_token_secret, consumer_key, consumer_secret, user_id)
-
 client = WithingsApi(creds)
 measures = client.get_measures()
 values = {}
 values["pulse"] = []
 values["ox"] = []
 for m in measures:
-	pulse = m.get_measure(11)
-	ox_sat = m.get_measure(54)
-	if pulse != None: 
-		values["pulse"].append((pulse, str(m.date)))
-	if ox_sat != None: 
-		values["ox"].append((ox_sat, str(m.date)))
+    pulse = m.get_measure(11)
+    ox_sat = m.get_measure(54)
+    if pulse is not None:
+        values["pulse"].append((pulse, str(m.date)))
+    if ox_sat is not None:
+        values["ox"].append((ox_sat, str(m.date)))
 print values
-	
-	
