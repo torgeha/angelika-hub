d81f363c983efceb84d6a2d38d5c8014056733ed
Torgeir Haaland <torgeha@gmail.com>
2014-11-13 15:22:38 +0100

    Update logger.py

--- hub/src/hub/logger.py
+++ hub/src/hub/logger.py
@@ -1,26 +1,17 @@
 import logging
-import logging.handlers
 from datetime import datetime as dt
 
 """
 Preferred way to print to console and log file. UTC-date is prepended.
+Only for debugging, file will grow forever, must be modified if used in production.
 """
-        
-import logging
-import logging.handlers
-from datetime import datetime as dt
-   
-log_filename = 'console_log.log'
-h_logger = logging.getLogger('hub_logger')
-h_logger.setLevel(logging.INFO)
-handler = logging.handlers.RotatingFileHandler(log_filename, maxBytes=1000000, backupCount=5)
-h_logger.addHandler(handler)
 
 def log_to_console(message):
-    now = dt.utcnow()
-    what_to_log =  "[UTC: " + now.strftime("%Y-%m-%d %H:%M:%S") + "] - " + message
-    print(what_to_log)
+        now = dt.utcnow()
+        what_to_log =  "[UTC: " + now.strftime("%Y-%m-%d %H:%M:%S") + "] - " + message
+        print(what_to_log)
         
-    h_logger.info(what_to_log)
+        logging.basicConfig(filename='console_log.log', level=logging.INFO)
+        logging.info(what_to_log)
 
         
